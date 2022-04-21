ef0b414d6a34acc8a90e9d921c040a2555bc5647
avyshamirski <avyshamirski@scnsoft.com>
2022-04-21 12:13:43 +0300

    feat: implement redux store

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,20 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import {applyMiddleware, combineReducers, createStore} from "redux";
+import thunk from "redux-thunk";
+import promise from "redux-promise-middleware";
+import {Provider} from "react-redux";
+import ReactDOM from 'react-dom';
+import {composeWithDevTools} from "@redux-devtools/extension";
 
-const root = ReactDOM.createRoot(
-    document.getElementById('root') as HTMLElement
-);
-root.render(
-    <React.StrictMode>
+const reducers = combineReducers({});
+export const store = createStore(reducers, composeWithDevTools(applyMiddleware(thunk, promise)));
+
+ReactDOM.render(
+    <Provider store={store}>
         <App/>
-    </React.StrictMode>
+    </Provider>,
+    document.getElementById('root')
 );
 
