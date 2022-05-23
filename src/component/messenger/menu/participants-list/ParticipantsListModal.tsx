b122b0cfbb1f0deb7578de6f7e3e8299906edb38
avyshamirski <avyshamirski@scnsoft.com>
2022-05-23 10:05:09 +0300

    feat: implement formik on permission form

--- src/component/messenger/menu/participants-list/ParticipantsListModal.tsx
+++ src/component/messenger/menu/participants-list/ParticipantsListModal.tsx
@@ -9,52 +9,50 @@ import ListItemText from "@mui/material/ListItemText/ListItemText";
 import StarIcon from '@mui/icons-material/Star';
 import PersonIcon from '@mui/icons-material/Person';
 import RemoveCircleIcon from '@mui/icons-material/RemoveCircle';
-import {AppState, useAppDispatch} from "../../../../index";
+import {AppState} from "../../../../index";
 import {Room} from "../../../../model/messenger/room/Room";
 import {setParticipantsListModalOpen} from "../../../../redux/messenger/messengerActions";
 import {retrieveUserId} from "../../../../service/authService";
 import {RoomService} from "../../../../service/messenger/room/RoomService";
 
 const ParticipantsListModal: React.FC<Props> = (props) => {
-    const dispatch = useAppDispatch();
 
     return (
         <Dialog open={props.isOpen} maxWidth="sm" fullWidth>
             <AppBar classes={{root: style.dialog__app_bar}}>
                 <Toolbar>
-                    <IconButton onClick={() => dispatch(setParticipantsListModalOpen(false))}>
+                    <IconButton onClick={() => props.setParticipantsListModalOpen(false)}>
                         <CloseIcon fontSize={'large'} classes={{root: style.dialog__close_icon}}/>
                     </IconButton>
-                    <Typography variant="h4" component="div" flex={1} mx={3}
-                                align={"center"}>{props.roomMembers.get(props.selectedRoom?.id)?.length} members</Typography>
+                    <Typography variant="h4" component="div" flex={1} mx={3} align={"center"}>
+                        {props.roomMembers.get(props.selectedRoom?.id)?.length} members
+                    </Typography>
                 </Toolbar>
             </AppBar>
             <List dense sx={{width: '100%'}} className={style.dialog__participants_list}>
-                {props.roomMembers.get(props.selectedRoom?.id)?.map((member) => {
-                    return (
-                        <ListItem
-                            key={member.id}
-                            secondaryAction={
-                                props.roomOwnerId === retrieveUserId() && props.roomOwnerId !== member.id && (
-                                    <IconButton
-                                        onClick={() => RoomService.kickUserFromRoom(props.selectedRoom?.id, member.id)}>
-                                        <RemoveCircleIcon className={style.dialog__remove_icon}/>
-                                    </IconButton>
-                                )
+                {props.roomMembers.get(props.selectedRoom?.id)?.map((member) => (
+                    <ListItem
+                        key={member.id}
+                        secondaryAction={
+                            props.roomOwnerId === retrieveUserId() && props.roomOwnerId !== member.id && (
+                                <IconButton
+                                    onClick={() => RoomService.kickUserFromRoom(props.selectedRoom?.id, member.id)}>
+                                    <RemoveCircleIcon className={style.dialog__remove_icon}/>
+                                </IconButton>
+                            )
+                        }
+                    >
+                        <ListItemIcon>
+                            {props.roomOwnerId === member.id
+                                ? <StarIcon fontSize={"medium"} className={style.dialog__star_icon}/>
+                                : <PersonIcon fontSize={"medium"} className={style.dialog__person_icon}/>
                             }
-                        >
-                            <ListItemIcon>
-                                {props.roomOwnerId === member.id
-                                    ? <StarIcon fontSize={"medium"} className={style.dialog__star_icon}/>
-                                    : <PersonIcon fontSize={"medium"} className={style.dialog__person_icon}/>
-                                }
-                            </ListItemIcon>
-                            <ListItemText>
-                                <p className={style.dialog__person_title}>{member.title}</p>
-                            </ListItemText>
-                        </ListItem>
-                    );
-                })}
+                        </ListItemIcon>
+                        <ListItemText>
+                            <p className={style.dialog__person_title}>{member.title}</p>
+                        </ListItemText>
+                    </ListItem>
+                ))}
             </List>
         </Dialog>
     );
@@ -67,7 +65,11 @@ const mapStateToProps = (state: AppState, ownProps: { selectedRoom: Room }) => (
     roomOwnerId: ownProps.selectedRoom?.ownerId
 })
 
-const connector = connect(mapStateToProps);
+const mapDispatchToProps = {
+    setParticipantsListModalOpen,
+}
+
+const connector = connect(mapStateToProps, mapDispatchToProps);
 
 type Props = ConnectedProps<typeof connector>
 
